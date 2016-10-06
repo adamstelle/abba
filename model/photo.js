ff2dc3eb272cbd28758bf4fcd1c8bf25bf369ea7
Sarah DeBey <sarah.debey@gmail.com>
2016-10-06 16:29:27 -0700

    adds user model

--- model/photo.js
+++ model/photo.js
@@ -1,15 +1,10 @@
 'use strict';
 
-// Photo
-// -  imageURI - passed back from AWS
-// -  Key - created by multer
-// -  UserID - reference
-
 const mongoose = require('mongoose');
 
 const photoSchema = mongoose.Schema({
   imageURI: {type: String, required: true},
-  Key: {type: String, required true},
+  Key: {type: String, required: true},
   userID: {type: mongoose.Schema.Types.ObjectId, required: true},
 });
 
