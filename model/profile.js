8fd7d359fcf10e70104a4bbe0183feabde905eaf
raziyeh bazargan <r.bazargan@gmail.com>
2016-10-06 16:29:18 -0700

    profile Model: create model

--- model/profile.js
+++ model/profile.js
@@ -10,7 +10,7 @@ const profileSchema = mongoose.Schema({
   firstName: {type: String, required: true},
   lastName: {type: String, required: true},
   phone: {type: String, required: true},
-  email: {type: String, required: true},
+  email: {type: String, required: true, unique: true},
   status: {type: String, required: true},
   userID: {type: mongoose.Schema.Types.ObjectId, required: true},
 });
