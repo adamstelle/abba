02f8032964de392752208cf907ec302522a8f9ec
Sarah DeBey <sarah.debey@gmail.com>
2016-10-07 15:09:39 -0700

    removed comments

--- test/lib/clean-up-mock.js
+++ test/lib/clean-up-mock.js
@@ -16,23 +16,3 @@ module.exports = function(done) {
   .then(() => done())
   .catch(done);
 };
-
-
-// 'use strict'
-//
-// const debug = require('debug')('slugram:clean-db')
-//
-// const Pic = require('../../model/pic.js')
-// const User = require('../../model/user.js')
-// const Gallery = require('../../model/gallery.js')
-//
-// module.exports = function(done){
-//   debug('clean up database')
-//   Promise.all([
-//     Pic.remove({}),
-//     User.remove({}),
-//     Gallery.remove({}),
-//   ])
-//   .then( () => done())
-//   .catch(done)
-// }
