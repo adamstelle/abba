9b1da2ed49292c41a09b03fc8efb0498ea5d44b3
Adam Stelle <adamstelle@gmail.com>
2016-10-11 08:37:25 -0700

    Refactor photo tests; all working

--- test/photo-router-test.js
+++ test/photo-router-test.js
@@ -35,57 +35,58 @@ describe('testing photo middleware', function(){
   before(done => serverControl.serverUp(server, done));
   after(done => serverControl.serverDown(server, done));
   afterEach(done => cleanUpDatabase(done));
-
-  describe('testing upload a photo', () => {
-    before(done => profileMock.call(this, done));
-    it('should return a photo', done => {
-      request.post(`localhost:3000/api/profile/${this.tempProfile._id}/photo`)
-      .set({
-        Authorization: `Bearer ${this.tempToken}`,
-      })
-      .field('name', examplePhoto.name)
-      .field('caption', examplePhoto.caption)
-      .attach('image', `${__dirname}/data/testpic.png`)
-      .then(res => {
-        Profile.findById(this.tempProfile._id)
-        .populate('photo')
-        .then(profile => {
-          expect(profile.photo.imageURI).to.equal(examplePhotoResult.imageURI);
-          expect(profile.photo.objectKey).to.equal(examplePhotoResult.objectKey);
-        });
-        expect(res.status).to.equal(200);
-        expect(res.body.caption).to.equal(examplePhoto.caption);
-        expect(res.body.imageURI).to.equal(examplePhotoResult.imageURI);
-        done();
-      })
-      .catch(done);
+  describe('testing POST /api/profile/:profileID/photo', () => {
+    describe('with valid body, auth and ID', () => {
+      before(done => profileMock.call(this, done));
+      it('should return a photo', done => {
+        request.post(`localhost:3000/api/profile/${this.tempProfile._id}/photo`)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`,
+        })
+        .field('name', examplePhoto.name)
+        .field('caption', examplePhoto.caption)
+        .attach('image', `${__dirname}/data/testpic.png`)
+        .then(res => {
+          Profile.findById(this.tempProfile._id)
+          .populate('photo')
+          .then(profile => {
+            expect(profile.photo.imageURI).to.equal(examplePhotoResult.imageURI);
+            expect(profile.photo.objectKey).to.equal(examplePhotoResult.objectKey);
+          });
+          expect(res.status).to.equal(200);
+          expect(res.body.caption).to.equal(examplePhoto.caption);
+          expect(res.body.imageURI).to.equal(examplePhotoResult.imageURI);
+          done();
+        })
+        .catch(done);
+      });
     });
   });
-  describe('testing delete a photo', () => {
-    before(done => photoMock.call(this, done));
-    it('should return a 204', done => {
-      request.delete(`localhost:3000/api/profile/${this.tempProfile._id}/photo/${this.tempPhoto._id}`)
-      .set({
-        Authorization: `Bearer ${this.tempToken}`,
-      })
-      .then(res => {
-        expect(res.status).to.equal(204);
-        done();
-      })
-      .catch(done);
-    });
-    before(done => photoMock.call(this, done));
-    it('should remove the photo from the profile', done => {
-      console.log('rpfoile is ', this.tempProfile._id);
-      request.delete(`localhost:3000/api/profile/${this.tempProfile._id}/photo/${this.tempPhoto._id}`)
-      .set({
-        Authorization: `Bearer ${this.tempToken}`,
-      })
-      .then(res => {
-        expect(res.status).to.equal(204);
-        done();
-      })
-      .catch(done);
+  describe('testing DELETE /api/profile/:profileID/photo/:id', () => {
+    describe('testing with valid auth and ID', () => {
+      beforeEach(done => photoMock.call(this, done));
+      it('should return a 204', done => {
+        request.delete(`localhost:3000/api/profile/${this.tempProfile._id}/photo/${this.tempPhoto._id}`)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`,
+        })
+        .then(res => {
+          expect(res.status).to.equal(204);
+          done();
+        })
+        .catch(done);
+      });
+      it('should remove the photo from the profile', done => {
+        request.delete(`localhost:3000/api/profile/${this.tempProfile._id}/photo/${this.tempPhoto._id}`)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`,
+        })
+        .then(res => {
+          expect(res.status).to.equal(204);
+          done();
+        })
+        .catch(done);
+      });
     });
   });
 });
